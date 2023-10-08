df8866c4228f7712185bf0e5b15d4ca951b9f0bd
Suyog Khanal <suyogkhanal@outlook.com>
2023-10-08 12:12:42 -0400

    final changes

--- clapCounter.py
+++ clapCounter.py
@@ -6,8 +6,9 @@ import math
 import random
 import time
 from playsound import playsound
+from datetime import datetime
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 detector = htm.handDetector(0.25)
 
 prevTime = 0
@@ -15,6 +16,8 @@ currTime = 0
 
 data = []
 becker = []
+timers = []
+
 
 for i in range(8):
     rand = random.randint(0, 1)
@@ -23,14 +26,33 @@ for i in range(8):
 detector = htm.handDetector()
 
 claps = 0
-clapre = []
 handstogether = False
 
-while True:
+for i in data:
+    temp = random.randint(2, 16)
+    rhy = float(temp) / 40
+    print(i)
+    if i == 1:
+        print(rhy)
+        becker.append(rhy)
+        playsound('audio.mp3')
+        time.sleep(rhy)
+    elif i == 0:
+        print(rhy)
+        becker.append(rhy)
+        playsound('audio.mp3')
+        time.sleep(rhy)
+
+tempo = [0.0] * 9
+run = True
+
+while run:
+    clapped = False
+    beeped = False
+    start = 0
     (success, img) = cap.read()
     img = detector.findHands(img)
     length = 0
-
     try:
         lmList1 = detector.findPosition(img, handNo=0)
         lmList2 = detector.findPosition(img, handNo=1)
@@ -40,23 +62,26 @@ while True:
 
         length = math.hypot(x2 - x1, y2 - y1)
 
-        if not handstogether and length < 75:  # a clap was seen
+        if not handstogether and length < 60:  # a clap was seen
+            tempo[claps] = datetime.now()
             claps += 1
-            clapre.append(1)
+            if claps == 1:
+                time.sleep(3)
             handstogether = True
-        elif handstogether and length > 90:
+        elif handstogether and length > 110:
 
                                             # a clap was not seen
 
-            clapre.append(0)
             handstogether = False
+
     except IndexError:
 
         if handstogether == False and claps > 0:
+            tempo[claps] = datetime.now()
             claps += 1
-            clapre.append(1)
+            if claps == 1:
+                time.sleep(3)
             handstogether = True
-
     cv2.putText(
         img,
         str(int(claps)),
@@ -76,8 +101,65 @@ while True:
         2,
         )
 
+    '''cv2.putText(
+        img,
+        str(timers),
+        (10, 170),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        (255, 255, 0),
+        2,
+        )'''
+
+    # cv2.putText(
+    #     img,
+    #     str(becker),
+    #     (10, 220),
+    #     cv2.FONT_HERSHEY_PLAIN,
+    #     3,
+    #     (255, 0, 0),
+    #     2,
+    #     )
     cv2.imshow('Image', img)
     cv2.waitKey(1)
+    end = time.time()
+    if claps >= 9:
+        run = False
 
-for i in clapre:
-    print(i)
+
+time_diffs = [(tempo[i] - tempo[i-1]).total_seconds() for i in range (1, len(tempo))]
+totaldiff = 0
+
+
+for i in range (len(becker)):
+    totaldiff += becker[i]
+for i in range (len(time_diffs)):
+        totaldiff -= time_diffs[i]
+
+totaldiff = abs(totaldiff) - 5
+score = 10000 - (1000 * totaldiff)
+if score < 0:
+    score = 0
+
+img = cv2.imread('elgato.jpg', cv2.IMREAD_ANYCOLOR)
+
+cv2.putText(
+        img,
+        'you were off by a total of ' + str(round(totaldiff,2)),
+        (40, 1000),
+        cv2.FONT_HERSHEY_PLAIN,
+        6,
+        (255, 0, 0),
+        10,
+        )
+cv2.putText(
+        img,
+        'SCORE:   ' + str(score),
+        (40, 500),
+        cv2.FONT_HERSHEY_PLAIN,
+        6,
+        (0, 0, 255),
+        10,
+        )
+cv2.imshow('Image', img)
+cv2.waitKey(0)
