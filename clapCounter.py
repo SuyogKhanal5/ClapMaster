4c2817858104c61ecf2144a3c5c0a59fdec77735
mannan <mannanshukla@icloud.com>
2023-10-08 06:14:11 -0400

    bruh

--- clapCounter.py
+++ clapCounter.py
@@ -1,47 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import HandTrackingModule as htm
 import cv2
 import math
+import random
+import time
+from playsound import playsound
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 detector = htm.handDetector(0.25)
 
 prevTime = 0
 currTime = 0
 
-cap = cv2.VideoCapture(1) 
+data = []
+becker = []
+
+for i in range(8):
+    rand = random.randint(0, 1)
+    data.append(rand)
 
 detector = htm.handDetector()
 
 claps = 0
+clapre = []
 handstogether = False
 
 while True:
-    success, img = cap.read()
+    (success, img) = cap.read()
     img = detector.findHands(img)
     length = 0
 
-    try:   
+    try:
         lmList1 = detector.findPosition(img, handNo=0)
         lmList2 = detector.findPosition(img, handNo=1)
 
-        x1, y1 = lmList1[9][1], lmList1[9][2]
-        x2, y2 = lmList2[9][1], lmList2[9][2]
+        (x1, y1) = (lmList1[9][1], lmList1[9][2])
+        (x2, y2) = (lmList2[9][1], lmList2[9][2])
 
         length = math.hypot(x2 - x1, y2 - y1)
 
-        if not handstogether and length < 75:
+        if not handstogether and length < 75:  # a clap was seen
             claps += 1
+            clapre.append(1)
             handstogether = True
         elif handstogether and length > 90:
-            handstogether = False
 
+                                            # a clap was not seen
+
+            clapre.append(0)
+            handstogether = False
     except IndexError:
-        if handstogether == False and claps > 0: 
+
+        if handstogether == False and claps > 0:
             claps += 1
+            clapre.append(1)
             handstogether = True
 
-    cv2.putText(img, str(int(claps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2) 
-    cv2.putText(img, str(int(length)), (10,120), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2) 
+    cv2.putText(
+        img,
+        str(int(claps)),
+        (10, 70),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        (0, 0, 255),
+        2,
+        )
+    cv2.putText(
+        img,
+        str(int(length)),
+        (10, 120),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        (255, 0, 0),
+        2,
+        )
 
-    cv2.imshow("Image", img) 
-    cv2.waitKey(1) 
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
+
+for i in clapre:
+    print(i)
--- test.py
+++ test.py
@@ -1,36 +1,29 @@
-import RPi.GPIO as GPIO
-import time
 import random
+import time
+from playsound import playsound
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(1,GPIO.OUT)
-servo = GPIO.PWM(1, 50)
 data = []
+becker = []
+# freq is set at 50
 
-for i in range(20):
-	rand = random.randint(0, 1)
+for i in range(8):
+	rand = random.randint(0,1)
 	data.append(rand)
 
 for i in data:
+	penis = random.randint(2,16)
+	rhy = float(penis) / 40
+	print(i)
+	if i == 1:
+		print(rhy)
+		becker.append(rhy)
+		playsound('audio.mp3')
+		time.sleep(rhy)
+	elif i == 0:
+		print(rhy)
+		becker.append(rhy)
+		time.sleep(rhy)
+
+for i in becker:
 	print(i)
 
-servo.start(0)
-print("waiting for one second")
-time.sleep(1)
-
-print("rotating at intervals of 12 degrees")
-duty = 2
-while duty <= 170:
-	servo.ChangeDutyCycle(duty)
-	time.sleep(1)
-	duty = duty+1
-
-print("turning back to 0")
-servo.ChangeDutyCycle(2)
-time.sleep(1)
-servo.ChangeDutyCycle(0)
-
-
-servo.stop()
-GPIO.cleanup()
-print("done!")
